3340f2ce178208add1c0d81482611e06df5d6e6e
mrziee <118080663+mrziee@users.noreply.github.com>
2022-11-27 22:22:13 +0700

    Update config.js

--- options/config.js
+++ options/config.js
@@ -31,16 +31,16 @@ global.APIKeys = {
 
 //━━━━━━━━━━━━━━━[ SETTING ]━━━━━━━━━━━━━━━━━//
 global.thumb = fs.readFileSync('./image/thumbnail.jpg') // JGN DI UBAH!
-global.name = 'Aulia Rahman (Owner)', // GNTI NAMA LU!
-global.creator = 'LiaaCans BOT' // GANTI NAMA BOT LU BANH!
-global.owner = ['6285821676621'] // GNTI NOMOR LU!
-global.premium = ['6285821676621', '6281528965381']
+global.name = 'Mr Ziee (Owner)', // GNTI NAMA LU!
+global.creator = 'Mr Ziee' // GANTI NAMA BOT LU BANH!
+global.owner = ['6285773914826'] // GNTI NOMOR LU!
+global.premium = ['6285773914826', '6289631157722']
  // ADD PREM YG LU MAU BANH!!
 global.quotes = 'Kesempatan Anda Untuk Sukses Di Setiap Kondisi Selalu Dapat Diukur Oleh Seberapa Besar Kepercayaan Anda Pada Diri Sendiri' // Ganti Quotes, Tapi Trsrh Klian
-global.pulsa = ['081528965381'] // UBAH NOMOR YG AKTIF!
-global.botname = 'Created LiaaCans BOT' // GNTI NAMA BOT LU!
+global.pulsa = ['0857 7391 4826'] // UBAH NOMOR YG AKTIF!
+global.botname = 'Ziee BOT' // GNTI NAMA BOT LU!
 global.packname = 'Nih Stikel Umtukmu' // TRSERAH MO GANTI AP!
-global.author = 'LiaaCans BOT' // GNTI NMA LU!
+global.author = 'Mr Ziee' // GNTI NMA LU!
 global.fake = 'LIAACANS BOT VERIFIED' // GNTI NMA BOT LU BANH!
 // FITUR TOPUP OTOMATIS 
 global.hargadmff = 1261
